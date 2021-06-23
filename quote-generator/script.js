1caeb5c297b8da6ad1670cb53ec0b08cf60504ae
unknown-cat <twilightcat73@yahoo.com>
2021-06-23 19:56:03 +0300

    Update(quote-generator): add quote text and author dynamically
    check quote length to determine styling
    check if author is null
    add ability to public quotes in twitter on button
    add event listeners for tweeter and new quote btn

--- quote-generator/script.js
+++ quote-generator/script.js
@@ -1,3 +1,9 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 // Get Quotes From Api
 let apiQuotes = [];
 
@@ -5,12 +11,25 @@ let apiQuotes = [];
 function newQuote() {
 	// Pick a random quote from apiQuotes array
 	const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-	console.log(quote)
+	if (!quote.author) {
+		authorText.textContent = 'Unknown Author'
+	} else {
+		authorText.textContent = quote.author
+	}
+
+	//Check Quote length to determine styling
+	if (quote.text.length > 120) {
+		quoteText.classList.add('long-quote');
+	} else {
+		quoteText.classList.remove('long-quote');
+	}
+
+	quoteText.textContent = quote.text;
 }
 
 async function getQuotes() {
 	const apiUrl = 'https://type.fit/api/quotes';
-	try	{
+	try {
 		const response = await fetch(apiUrl);
 		apiQuotes = await response.json();
 		newQuote();
@@ -18,6 +37,16 @@ async function getQuotes() {
 		// Catch Error Here
 	}
 }
+
+// Tweet Quote
+function tweetQuote() {
+	const tweeterUrl = `https://twitter.com/intent/tweet?text=${ quoteText.textContent } - ${ authorText.textContent }`;
+	window.open(tweeterUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', newQuote)
+twitterBtn.addEventListener('click', tweetQuote)
 
 // On Load;
 getQuotes();
