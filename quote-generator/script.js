9c9fc0191fe88a19b9608ada66306409984c883a
unknown-cat <twilightcat73@yahoo.com>
2021-06-25 18:05:00 +0300

    Update(quote-generator): add loader functions

--- quote-generator/script.js
+++ quote-generator/script.js
@@ -3,12 +3,25 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
-// Get Quotes From Api
 let apiQuotes = [];
+
+//Show Loading
+function loading() {
+	loader.hidden = false;
+	quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function complete() {
+	quoteContainer.hidden = false;
+	loader.hidden = true;
+}
 
 // Show New Quote
 function newQuote() {
+	loading();
 	// Pick a random quote from apiQuotes array
 	const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
 	if (!quote.author) {
@@ -24,10 +37,14 @@ function newQuote() {
 		quoteText.classList.remove('long-quote');
 	}
 
+	//Set Quote, Hide Loader
 	quoteText.textContent = quote.text;
+	complete();
 }
 
+// Get Quotes From Api
 async function getQuotes() {
+	loading();
 	const apiUrl = 'https://type.fit/api/quotes';
 	try {
 		const response = await fetch(apiUrl);
