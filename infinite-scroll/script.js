e6742b77ed7a2105d07588906a2263c677790525
unknown-cat <twilightcat73@yahoo.com>
2021-06-28 21:28:59 +0300

    Update(infinite scroll): add helper function to set attributes
     add helper function to set attributes on DOM elements

--- infinite-scroll/script.js
+++ infinite-scroll/script.js
@@ -8,19 +8,30 @@ const count = 10;
 const apiKey = 'h-dKN3vNa2XZX7ER0sGRAb24rOBoqfHijCq4YpsGHlE';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${ apiKey }&count=${ count }`
 
+// Helper Function To Set Attributes on DOM Elements
+function setAttributes(element, attributes) {
+	for(const key in attributes) {
+		element.setAttribute(key, attributes[key])
+	}
+}
+
 //Create Elements for Links & Photos, Add to DOM
 function displayPhotos() {
 	//Run function for each object in photosArray
 	photosArray.forEach(photo => {
 		// Create <a> to link Unsplash
 		const item = document.createElement('a');
-		item.setAttribute('href', photo.links.html);
-		item.setAttribute('target', '_blank');
+		setAttributes(item, {
+			href: photo.links.html,
+			target: '_blank'
+		});
 		// Create <img> to display photo
 		const img = document.createElement('img');
-		img.setAttribute('src', photo.urls.regular);
-		img.setAttribute('alt', photo.alt_description);
-		img.setAttribute('title', photo.alt_description);
+		setAttributes(img, {
+			src: photo.urls.regular,
+			alt: photo.alt_description,
+			title: photo.alt_description
+		})
 		// Put <img> inside <a>, then put both inside imageContainer Element
 		item.appendChild(img)
 		imageContainer.appendChild(item)
