c4736b74781b0281e33004b3e17ab2e96ad6f322
unknown-cat <twilightcat73@yahoo.com>
2021-10-03 16:26:12 +0300

    feat: animate progress bar

--- music-player/script.js
+++ music-player/script.js
@@ -2,6 +2,8 @@ const image = document.querySelector('img')
 const title = document.getElementById('title')
 const artist = document.getElementById('artist')
 const music = document.querySelector('audio')
+const progressContainer = document.getElementById('progress-container')
+const progress = document.getElementById('progress')
 const playBtn = document.getElementById('play')
 const prevBtn = document.getElementById('prev')
 const nextBtn = document.getElementById('next')
@@ -40,6 +42,7 @@ const playSong = () => {
 	playBtn.setAttribute('title', 'Play')
 	music.play()
 }
+
 //Pause
 const pauseSong = () => {
 	isPlaying = false
@@ -82,6 +85,17 @@ const nextSong = () => {
 // On Load Select First Song
 loadSong(songs[songIndex])
 
+// Update Progress Bar & Time
+const updateProgressBar = (e) => {
+	if (isPlaying) {
+		const { duration, currentTime } = e.srcElement
+		// Upgrade Progress Bar Width
+		const progressPercent = (currentTime / duration) * 100;
+		progress.style.width = `${progressPercent}%`
+	}
+}
+
 // Event Listeners
 prevBtn.addEventListener('click', prevSong)
 nextBtn.addEventListener('click', nextSong)
+music.addEventListener('timeupdate', updateProgressBar)
