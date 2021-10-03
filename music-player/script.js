a83a21d723d12ba899bdcefa99ef951804ce41e0
unknown-cat <twilightcat73@yahoo.com>
2021-10-03 14:23:00 +0300

    feat: add ability to change songs on prev & next btn

--- music-player/script.js
+++ music-player/script.js
@@ -1,7 +1,34 @@
+const image = document.querySelector('img')
+const title = document.getElementById('title')
+const artist = document.getElementById('artist')
 const music = document.querySelector('audio')
 const playBtn = document.getElementById('play')
 const prevBtn = document.getElementById('prev')
 const nextBtn = document.getElementById('next')
+
+// Music
+const songs = [
+	{
+		name: 'jacinto-1',
+		displayName: 'Electric Chill Machine',
+		artist: 'Jacinto Design',
+	},
+	{
+		name: 'jacinto-2',
+		displayName: 'Seven Nation Army (Remix)',
+		artist: 'Jacinto Design',
+	},
+	{
+		name: 'jacinto-3',
+		displayName: 'Goodnight, Disco Queen',
+		artist: 'Jacinto Design',
+	},
+	{
+		name: 'metric-1',
+		displayName: 'Front Row (Remix)',
+		artist: 'Metric/Jacinto Design',
+	},
+]
 
 // Check if Playing
 let isPlaying = false
@@ -20,12 +47,41 @@ const pauseSong = () => {
 	playBtn.setAttribute('title', 'Pause')
 	music.pause()
 }
-//Next
-const nextSong = () => {
-}
-//Prev
-const prevSong = () => {
-}
 
 // Play or Pause Event Listener
-playBtn.addEventListener('click', () => isPlaying ? pauseSong() : playSong())
+playBtn.addEventListener('click', () => isPlaying ? pauseSong()
+	: playSong())
+
+// Update DOM
+const loadSong = (song) => {
+	title.textContent = song.displayName
+	artist.textContent = song.artist
+	music.src = `music/${ song.name }.mp3`
+	image.src = `img/${ song.name }.jpg`
+}
+
+// Current Song
+let songIndex = 0
+
+//Prev Song
+const prevSong = () => {
+	songIndex--
+	if (songIndex < 0) songIndex = songs.length - 1
+	loadSong(songs[songIndex])
+	playSong()
+}
+
+//Next Song
+const nextSong = () => {
+	songIndex++
+	if (songIndex > songs.length - 1) songIndex = 0
+	loadSong(songs[songIndex])
+	playSong()
+}
+
+// On Load Select First Song
+loadSong(songs[songIndex])
+
+// Event Listeners
+prevBtn.addEventListener('click', prevSong)
+nextBtn.addEventListener('click', nextSong)
