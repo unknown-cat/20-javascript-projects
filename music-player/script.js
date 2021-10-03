cc196f8c9bfb7225601229c65912814076f4e19f
unknown-cat <twilightcat73@yahoo.com>
2021-10-03 17:13:51 +0300

    feat: add ability to play next song if current is ended and update progress bar on click on progress bar

--- music-player/script.js
+++ music-player/script.js
@@ -113,7 +113,17 @@ const updateProgressBar = (e) => {
 	}
 }
 
+// Set Progress Bar
+const setProgressBar = (e) => {
+	const width = e.srcElement.clientWidth
+	const clickX = e.offsetX
+	const { duration } = music
+	music.currentTime = (clickX / width) * duration
+}
+
 // Event Listeners
 prevBtn.addEventListener('click', prevSong)
 nextBtn.addEventListener('click', nextSong)
+music.addEventListener('ended', nextSong)
 music.addEventListener('timeupdate', updateProgressBar)
+progressContainer.addEventListener('click', setProgressBar)
