6edc6b6b2f534d46148307090d24fa6f4b93f4c9
unknown-cat <twilightcat73@yahoo.com>
2021-10-16 21:09:26 +0300

    feat: add ability to store countdown title and date to local storage

--- custom-countdown/script.js
+++ custom-countdown/script.js
@@ -15,6 +15,7 @@ let countdownTitle = ''
 let countdownDate = ''
 let countdownValue = Date
 let countdownActive
+let savedCountdown;
 
 const second = 1000
 const minute = second * 60
@@ -30,13 +31,10 @@ function updateDOM() {
 	countdownActive = setInterval(() => {
 		const now = new Date().getTime()
 		const distance = countdownValue - now
-		console.log(distance)
-
 		const days = Math.floor(distance / day)
 		const hours = Math.floor((distance % day) / hour)
 		const minutes = Math.floor((distance % hour) / minute)
 		const seconds = Math.floor((distance % minute) / second)
-		console.log(days, hours, minutes, seconds)
 		// Hide Input
 		inputContainer.hidden = true
 		//If countdown has ended. show complete
@@ -63,6 +61,11 @@ function updateCountdown(e) {
 	e.preventDefault()
 	countdownTitle = e.srcElement[0].value
 	countdownDate = e.srcElement[1].value
+	savedCountdown = {
+		title: countdownTitle,
+		date: countdownDate
+	}
+	localStorage.setItem('countdown', JSON.stringify(savedCountdown))
 	// Check for valid date
 	if (countdownDate === '') {
 		alert('Please select a date for the countdown.')
@@ -84,9 +87,25 @@ function reset() {
 	// Reset Values
 	countdownTitle = ''
 	countdownDate = ''
+	localStorage.removeItem('countdown')
+}
+
+function restorePreviousCountdown() {
+	// Get countdown from localStorage if available
+	if (localStorage.getItem('countdown')) {
+		inputContainer.hidden = true
+		savedCountdown = JSON.parse(localStorage.getItem('countdown'))
+		countdownTitle = savedCountdown.title
+		countdownDate = savedCountdown.date
+		countdownValue = new Date(countdownDate).getTime()
+		updateDOM()
+	}
 }
 
 // Event Listeners
 countdownForm.addEventListener('submit', updateCountdown)
 countdownButton.addEventListener('click', reset)
 completeButton.addEventListener('click', reset)
+
+// On Load, check LocalStorage
+restorePreviousCountdown()
