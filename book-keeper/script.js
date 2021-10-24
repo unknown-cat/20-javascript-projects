3833fdf16e7ff5ae57e2e03557fcc197ece7d970
unknown-cat <twilightcat73@yahoo.com>
2021-10-24 14:06:53 +0300

    feat: build bookmarks DOM using js and delete static HTML bookmarks UI

--- book-keeper/script.js
+++ book-keeper/script.js
@@ -39,6 +39,40 @@ function validate(nameValue, urlValue) {
 	return true
 }
 
+// Build Bookmarks DOM
+function buildBookmarks() {
+	// Build items
+	bookmarks.forEach(bookmark => {
+		const { name, url } = bookmark;
+		// Item
+		const item = document.createElement('li')
+		item.classList.add('item')
+		// Close Icon
+		const closeIcon = document.createElement('i')
+		closeIcon.classList.add('fas', 'fa-times', 'delete-bookmark')
+		closeIcon.setAttribute('title', 'Delete Bookmark')
+		closeIcon.setAttribute('onclick', `deleteBookmarks('${ url }')`)
+		// Favicon / Link Container
+		const linkInfo = document.createElement('div')
+		linkInfo.classList.add('item-name')
+		// Favicon
+		const favicon = document.createElement('img')
+		favicon.classList.add('item-picture')
+		favicon.setAttribute('src', `https://s2.googleusercontent.com/s2/favicons?domain=${ url }`)
+		favicon.setAttribute('alt', 'Favicon')
+		// Link
+		const link = document.createElement('a')
+		link.classList.add('item-text')
+		link.setAttribute('href', `${ url }`)
+		link.setAttribute('target', '_blank')
+		link.textContent = name
+		// Append to Bookmarks container
+		linkInfo.append(favicon, link)
+		item.append(closeIcon, linkInfo)
+		bookmarksContainer.appendChild(item)
+	})
+}
+
 // Fetch Bookmarks
 function fetchBookmarks() {
 	// Get bookmarks from localStorage if available
@@ -54,7 +88,7 @@ function fetchBookmarks() {
 		]
 		localStorage.setItem('bookmarks', JSON.stringify(bookmarks))
 	}
-	console.log(bookmarks)
+	buildBookmarks()
 }
 
 // Handle Data from Form
