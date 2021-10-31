e78689ccc9f15a4f1927cc47a7307613fbe27909
unknown-cat <twilightcat73@yahoo.com>
2021-10-31 15:51:29 +0300

    feat: add ability to click on progress bar and set current time of the video

--- video-player/script.js
+++ video-player/script.js
@@ -43,8 +43,16 @@ function displayTime(time) {
 // Update progress bar as video plays
 function updateProgress() {
 	progressBar.style.width = `${ (video.currentTime / video.duration) * 100 }%`
-	currentTime.textContent = `${displayTime(video.currentTime)}`
-	duration.textContent = `${displayTime(video.duration)}`
+	currentTime.textContent = `${ displayTime(video.currentTime) }`
+	duration.textContent = `${ displayTime(video.duration) }`
+}
+
+// Click to seek within the video
+function setProgress(e) {
+	const newTime = e.offsetX / progressRange.offsetWidth
+	progressBar.style.width = `${ newTime * 100 }`
+	video.currentTime = newTime * video.duration
+	console.log(newTime)
 }
 
 // Volume Controls --------------------------- //
@@ -61,3 +69,4 @@ playBtn.addEventListener('click', togglePlay)
 video.addEventListener('click', togglePlay)
 video.addEventListener('timeupdate', updateProgress)
 video.addEventListener('canplay', updateProgress)
+progressRange.addEventListener('click', setProgress)
