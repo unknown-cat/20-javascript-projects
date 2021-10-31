4390c53e2c819161e77555cb36093db1d3538227
unknown-cat <twilightcat73@yahoo.com>
2021-10-31 16:27:41 +0300

    feat: add ability to mute/unmute on video plaing

--- video-player/script.js
+++ video-player/script.js
@@ -55,6 +55,7 @@ function setProgress(e) {
 }
 
 // Volume Controls --------------------------- //
+let lastVolume = 1
 
 // Volume Bar
 function changeVolume(e) {
@@ -73,6 +74,24 @@ function changeVolume(e) {
 	} else if (volume === 0) {
 		volumeIcon.classList.add('fas', 'fa-volume-off')
 	}
+	lastVolume = volume;
+}
+
+// Mute/Unmute
+function toggleMute() {
+	volumeIcon.className = ''
+	if (video.volume) {
+		lastVolume = video.volume
+		video.volume = 0;
+		volumeBar.style.width = 0
+		volumeIcon.classList.add('fas', 'fa-volume-mute')
+		volumeIcon.setAttribute('title', 'Unmute')
+	} else {
+		video.volume = lastVolume
+		volumeBar.style.width = `${ lastVolume * 100 }%`
+		volumeIcon.classList.add('fas', 'fa-volume-up')
+		volumeIcon.setAttribute('title', 'Mute')
+	}
 }
 
 // Change Playback Speed -------------------- //
@@ -88,3 +107,4 @@ video.addEventListener('timeupdate', updateProgress)
 video.addEventListener('canplay', updateProgress)
 progressRange.addEventListener('click', setProgress)
 volumeRange.addEventListener('click', changeVolume)
+volumeIcon.addEventListener('click', toggleMute)
