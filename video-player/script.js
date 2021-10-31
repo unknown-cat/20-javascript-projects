dc8914e4789bc110e7efd976242ff3fc00c53dab
unknown-cat <twilightcat73@yahoo.com>
2021-10-31 14:18:47 +0300

    feat: add abulity to play/pause a video on the play btn or video element

--- video-player/script.js
+++ video-player/script.js
@@ -1,21 +1,47 @@
-
+const video = document.querySelector('.video')
+const progressRange = document.querySelector('.progress-range')
+const progressBar = document.querySelector('.progress-bar')
+const playBtn = document.querySelector('#play-btn')
+const volumeRange = document.querySelector('.volume-range')
+const volumeIcon = document.querySelector('.volume-icon')
+const volumeBar = document.querySelector('.volume-bar')
+const currentTime = document.querySelector('.time-elapsed')
+const fullscreenBtn = document.querySelector('.fullscreen')
+const duration = document.querySelector('.time-duration')
 
 // Play & Pause ----------------------------------- //
 
+function showPlayIcon() {
+	playBtn.classList.replace('fa-pause', 'fa-play')
+	playBtn.setAttribute('title', 'Play')
+}
 
+function togglePlay() {
+	if (video.paused) {
+		video.play()
+		playBtn.classList.replace('fa-play', 'fa-pause')
+		playBtn.setAttribute('title', 'Pause')
+	} else {
+		video.pause()
+		showPlayIcon()
+	}
+}
+
+// On Video End, show play button icon
+video.addEventListener('ended', showPlayIcon)
 
 // Progress Bar ---------------------------------- //
-
 
 
 // Volume Controls --------------------------- //
 
 
-
 // Change Playback Speed -------------------- //
-
 
 
 // Fullscreen ------------------------------- //
 
 
+// Event Listeners
+playBtn.addEventListener('click', togglePlay)
+video.addEventListener('click', togglePlay)
