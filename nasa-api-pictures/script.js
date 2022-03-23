89be99eecbe5f0aa6a90d42eb06ab21db9e44064
unknown-cat <twilightcat73@yahoo.com>
2022-03-23 20:36:09 +0300

    feat: add storage for favorites and save it to localstorage
    also show added popup, when click on add to favorites

--- nasa-api-pictures/script.js
+++ nasa-api-pictures/script.js
@@ -10,6 +10,7 @@ const apiKey = 'DEMO_KEY';
 const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${count}`;
 
 let resultsArray = [];
+let favorites = {};
 
 function updateDOM() {
   resultsArray.forEach((result) => {
@@ -38,6 +39,7 @@ function updateDOM() {
     const saveText = document.createElement('p');
     saveText.classList.add('clickable');
     saveText.textContent = 'Add To Favorites';
+    saveText.setAttribute('onclick', `saveFavorites('${result.url}')`)
     // Card Text
     const cardText = document.createElement('p');
     cardText.classList.add('card-text');
@@ -70,6 +72,22 @@ async function getNasaPictures() {
   } catch (error) {
     // Catch Error Here
   }
+}
+
+// Add result To Favorite
+function saveFavorites(itemUrl) {
+  // Loop through Results Array to select Favorite
+  resultsArray.forEach((item) => {
+    if (item.url.includes(itemUrl) && !favorites[itemUrl]) {
+      favorites[itemUrl] = item;
+      saveConfermed.hidden = false; // Show That Item Was Added
+      setTimeout(() => {
+        saveConfermed.hidden = true;
+      }, 2000);
+      // Set Favorites To LocalStorage
+      localStorage.setItem('nasaFavorites', JSON.stringify(favorites))
+    }
+  })
 }
 
 // On Load
